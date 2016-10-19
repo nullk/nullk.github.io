bebbcb3cd09abd6fbb47e4099b8dc0d8de34f01e
nullak <nullk@protonmail.ch>
2016-10-19 16:22:10 -0400

    Warn user of slow manipulation w/ high res images

--- js/main.js
+++ js/main.js
@@ -8,7 +8,11 @@ var bg_canvas = document.getElementById('bg_canvas');
 var bg_ctx = bg_canvas.getContext('2d');
 
 var author_color;
+// file reader
+var reader;
+var cover_photo = new Image();
 
+// default properties of the book cover photo 
 var cover_properties = {
     x: 0,
     y: 0,
@@ -17,6 +21,7 @@ var cover_properties = {
 };
 
 function update(ctx, canvas) {
+    // updates the following at 60fps 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     draw_template();
     draw_author(ctx, author_color);
@@ -24,40 +29,41 @@ function update(ctx, canvas) {
 }
 
 function scale_image(photo_canvas, photo_ctx) {
+    // width/height HTML ranges call this function 
+    // to scale the image
     width = document.getElementById("width_range").value;
     height = document.getElementById("height_range").value;
 
     cover_properties['width'] = 10 * width;
     cover_properties['height'] = 10 * height;
 
-    update_cover(photo_canvas, photo_ctx, cover_properties['x'], cover_properties['y'],
+    update_cover(cover_photo, photo_canvas, photo_ctx, cover_properties['x'], cover_properties['y'],
         cover_properties['width'], cover_properties['height']);
 }
 
 function translate_image(canvas, photo_ctx) {
+    // x/y HTML ranges call this function 
+    // to move the image
     var x = document.getElementById("x_range").value;
     var y = document.getElementById("y_range").value;
 
     cover_properties['x'] = 10 * x;
     cover_properties['y'] = -(5 * y);
 
-    update_cover(photo_canvas, photo_ctx, cover_properties['x'], cover_properties['y'],
+    update_cover(cover_photo, photo_canvas, photo_ctx, cover_properties['x'], cover_properties['y'],
         cover_properties['width'], cover_properties['height']);
 }
 
-function update_cover(photo_canvas, photo_ctx, x, y, width, height) {
-    photo_ctx.clearRect(0, 0, photo_canvas.width, photo_canvas.height);
-
-    var cover_photo = new Image();
+function load_cover(reader, cover_photo) {
     var file = document.querySelector('input[type=file]').files[0];
-    var reader = new FileReader();
+    reader = new FileReader();    
 
     reader.onloadend = function() {
         cover_photo.src = reader.result;
-    };
-    cover_photo.onload = function() {
-        photo_ctx.drawImage(this, x, y, width, height);
-    };
+        if (cover_photo.width > 1500 || cover_photo.height > 1500) {
+            Materialize.toast('Warning: Large image files may be slow to manipulate!', 4000);
+        }
+    };    
 
     if (file) {
         if (file.type == 'image/jpeg' || file.type == 'image/png') {
@@ -69,7 +75,17 @@ function update_cover(photo_canvas, photo_ctx, x, y, width, height) {
     }
 }
 
+function update_cover(cover_photo, photo_canvas, photo_ctx, x, y, width, height) {
+    photo_ctx.clearRect(0, 0, photo_canvas.width, photo_canvas.height);
+    photo_ctx.drawImage(cover_photo, x, y, width, height);
+
+    cover_photo.onload = function() {
+        photo_ctx.drawImage(this, x, y, width, height);
+    };
+}
+
 function draw_template() {
+    // draws the base template
     var ctx = document.getElementById('bg_canvas').getContext('2d');
     var template = new Image();
     template.onload = function() {
@@ -111,6 +127,8 @@ function draw_title(ctx) {
 }
 
 function download_canvas(bg_canvas, photo_canvas, canvas) {
+    // create a canvas (dl_canvas) that merges all three canvases 
+    // into one then converts it to an image/png 
     var dl_canvas = document.getElementById('dl_canvas');
     var dl_ctx = dl_canvas.getContext('2d');
 
@@ -120,10 +138,13 @@ function download_canvas(bg_canvas, photo_canvas, canvas) {
 
     var imageURL = dl_canvas.toDataURL("image/png");
 
+    // clear dl_canvas
     dl_ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // notify the user
     Materialize.toast('Downloading!', 4000);
 
+    // a download link is created and opened 
     var dl_link = document.createElement('a');
     dl_link.href = imageURL;
     dl_link.download = document.getElementById('title').value + '.png';
@@ -132,6 +153,7 @@ function download_canvas(bg_canvas, photo_canvas, canvas) {
 }
 
 window.onload = function() {
+    // called when the page is first opened
     setInterval("update(ctx, canvas)", 1000 / 60);
     author_color = '#D28928';
 };
